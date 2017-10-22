534404ae9963c84d96943bb4eac7e88f46e05532
Brian Kramer <subsoap@gmail.com>
2017-10-22 14:41:14 -0700

    speech labels

---
+++ 2d_fov_field_of_view/main/gos/speech_factory.go
@@ -0,0 +1,38 @@
+embedded_components {
+  id: "factory"
+  type: "factory"
+  data: "prototype: \"/main/gos/speech.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/main/assets/atlases/assets.atlas\"\n"
+  "default_animation: \"dot_red\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: -100.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
